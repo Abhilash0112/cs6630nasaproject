18cead94cb2d4790a59d472a5b1933e985cf7cc6
diana ngo <diana.ngo@utah.edu>
2017-11-07 21:55:17 -0700

    Added map data and svg.

--- finalproject/public/js/map.js
+++ finalproject/public/js/map.js
@@ -3,5 +3,31 @@ class Map {
      * Constructor for the Map
      */
 	constructor() {
+		this.projection = d3.geoNaturalEarth1().scale(150);
     };
+	
+	/**
+     * Renders the map
+     * @param JSON data representing all countries
+     */
+    drawMap(world) {
+		let features = topojson.feature(world, world.objects.countries)
+			.features;
+		let map = d3.select("#map");
+		let path = d3.geoPath()
+			.projection(this.projection);
+		let graticule = d3.geoGraticule();
+		map.selectAll("path")
+			.data(features)
+			.enter()
+			.append("path")
+			.attr("id", function(d) {
+				return d.id;
+			})
+			.attr("d", path);
+		d3.select("#graticules")
+			.append("path")
+			.datum(graticule)
+			.attr("d", path);
+	}
 }
--- finalproject/public/js/script.js
+++ finalproject/public/js/script.js
@@ -1,6 +1,11 @@
 //load the data
 d3.csv("data/data.csv", function(error, data) {
 	let map = new Map();
+	d3.json("data/world.json", function (error, world) {
+        if (error) throw error;
+        map.drawMap(world);
+    });
+	
 	let timeline = new Timeline();
 	let table = new Table();
 });
